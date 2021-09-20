c98a6a5cbefed432d2005eeba7312c549ce5bfac
pruthvi7384 <pruthvirajrajput575@gmail.com>
2021-09-20 12:49:12 +0530

    hostel management system

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Route, Switch } from "react-router";
+import SinginForm from "./components/pages/SinginForm";
+import './index.css';
+import Dashboard from "./components/pages/Dashboard";
+import StaffPage from "./components/pages/StaffPage";
+import BranchesPage from "./components/pages/BranchesPage";
+import ApporitmentPage from "./components/pages/ApporitmentPage";
+import PastiontPage from "./components/pages/PastiontPage";
+import DoctorPage from "./components/pages/DoctorPage";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/">
+          <SinginForm/>
+      </Route>
+      <Route exact path="/dashboard">
+          <Dashboard/>
+      </Route>
+      <Route exact path="/doctors">
+        <DoctorPage/>
+      </Route>
+      <Route exact path="/staff">
+          <StaffPage/>
+      </Route>
+      <Route exact path="/branches">
+        <BranchesPage/>
+      </Route>
+      <Route exact path="/apporitment">
+          <ApporitmentPage/>
+      </Route>
+      <Route exact path="/patients">
+        <PastiontPage/>
+      </Route>
+    </Switch>
   );
 }
 
